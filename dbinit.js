79232a9cfc9789cc153e715fd49b37eaf5511699
feychou <federica.recanatini@gmail.com>
2021-09-09 11:29:09 +0200

    Update to newest mongoose version

--- dbinit.js
+++ dbinit.js
@@ -1,16 +1,8 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  const options = {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: true,
-    useUnifiedTopology: true
-  };
 
-  const conn = await mongoose.connect(process.env.MONGO_URI, options);
-  //conn.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
-  
+  const conn = await mongoose.connect(process.env.MONGO_URI);
   console.log(`Mongo DB connected ${conn.connection.host}`.yellow.underline);
 }
 
